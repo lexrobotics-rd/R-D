8d0251a8c6892fc329dd67bb4ae6856f4723eecd
Clive C <doobahead@gmail.com>
2014-10-15 22:07:08 -0400

    Self-diagnostics. Just a random idea.

---
+++ SelfDiagnostics.c
@@ -0,0 +1,19 @@
+/*
+SelfDiagnostics.c
+A series of tests to ensure that the robot is ready for competition. Stand-alone program.
+
+LastUpdatedOn: 10-15-14
+LastUpdatedBy: Clive
+Status: Just barely started
+*/
+
+
+/* HOW TO USE IT (Don't know yet) */
+
+
+task main(){
+	//check battery level
+	//check that all the necessary motors actually are there; flex them one by one for a supervisor to confirm
+	//check that necessary files are there? Does that work?
+	//play some music ^^
+}
