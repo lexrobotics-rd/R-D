b827f76b83fadf122a52ccf6d478c857c3e0bd58
Clive C <doobahead@gmail.com>
2014-10-22 16:03:19 -0400

    Created a test file for nonviolence.

---
+++ NonviolenceTest.c
@@ -0,0 +1,18 @@
+
+
+#include "Nonviolence.c"
+
+task main(){
+	motor[motorA] = 0;
+	wait10Msec(100);
+	motor[motorA] = 100;
+	wait10Msec(100);
+	motor[motorA] = 0;
+	wait10Msec(100);
+
+	startTask(nonviolenceTask);
+	nonviolence(motorA,100);
+	wait10Msec(100);
+	nonviolence(motorA,0);
+	wait10Msec(100);
+}
