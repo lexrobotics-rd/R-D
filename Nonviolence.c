589e373871580e6702b80a343e888c4f0fe84a74
Robotics <coding@lexrobotics.com>
2014-10-22 16:57:58 -0400

    NV works!!! now try logistic speed, distance-find Try exponential, logistic, etc paths. Also, does it integrate nicely? Can this be made into a more general library for logistically getting to a position/velocity/etc. target?

--- Nonviolence.c
+++ Nonviolence.c
@@ -23,24 +23,25 @@ Note also that the NXT is not thread-safe, even though it has multiple "threads"
 
 
 // STUFF YOU MIGHT WANT TO DEFINE
-const int NV_trans = 500; //Imagine it's like a CSS transition, except with motor velocity. Milliseconds.
-const int NV_wait = 50;//Milliseconds, how long to wait between adjustments. High = faster NXT program but choppier.
+const int NV_trans = 3000; //Imagine it's like a CSS transition, except with motor velocity. Milliseconds.
+const int NV_wait = 100;//Milliseconds, how long to wait between adjustments. High = faster NXT program but choppier.
 
 
 // STUFF YOU SHOULDN'T TOUCH
 const float NV_incfactor = ((float)NV_wait) / ((float)NV_trans); //the constant increment used to scale acceleration.
-typedef short motorType;
+//typedef short tMotor;
 const int numMotors = kNumbOfTotalMotors;
+const tMotor NV_sentinel = (tMotor)122;//-------------this is really sketchy-------------
 
 bool NV_mode_nonviolent = true;
 
-motorType NV_mtrs[numMotors];  //declares array of motor numbers ("motorsLeft" and "motorsRight" are actually stored as integers)
-int NV_targets[numMotors]; //declares array of target speeds for each motor
-int NV_incs[numMotors];  //declares array of speed increments for each motor
+tMotor NV_mtrs[numMotors];  //declares array of motor numbers ("motorsLeft" and "motorsRight" are actually stored as integers)
+signed int NV_targets[numMotors]; //declares array of target speeds for each motor
+signed int NV_incs[numMotors];  //declares array of speed increments for each motor
 
-void nonviolence(motorType newInputMotor, int target){	//this function is called in order to add a motor to the next
+void nonviolence(tMotor newInputMotor, int target){	//this function is called in order to add a motor to the next
 	for(int i = 0; i < numMotors; i++){ 										//available slot in the nonviolenceTask process queue
-		if(newInputMotor == NV_mtrs[i] || NV_mtrs[i] == -1){
+		if(newInputMotor == NV_mtrs[i] || NV_mtrs[i] == NV_sentinel){
 			NV_mtrs[i] = newInputMotor;
 			NV_targets[i] = target;
 			NV_incs[i] = (target - motor[newInputMotor]) * NV_incfactor;
@@ -49,14 +50,14 @@ void nonviolence(motorType newInputMotor, int target){	//this function is called
 	}
 }
 
-void nonviolenceMode(string s){
+void nonviolenceMode(const string s){
 	if(s=="gandhi")NV_mode_nonviolent = true;
 	else if(s=="hannibal")NV_mode_nonviolent = false;
 }
 
 task nonviolenceTask{
 	for(int i = 0; i < numMotors; i++){ //--------Initializes Nonviolence data arrays-------//
-		NV_mtrs[i] = -1;//Sentinel for the NV_mtrs[] array. Considered an empty value.
+		NV_mtrs[i] = NV_sentinel;//Sentinel for the NV_mtrs[] array. Considered an empty value.
 		NV_targets[i] = 0;//Target will be set by nonviolence() anyway.
 		NV_incs[i] = 0;//Inc will also be set by nonviolence().
 	}
@@ -64,8 +65,8 @@ task nonviolenceTask{
 	float diff;
 	while(true){
 		for(int i = 0; i < numMotors; i++){//Check on all the motors, inc if necessary.
-			if(NV_mtrs[i] == -1) break; //Ok, we've reached the sentinel -- all the remaining empty spots are filled with -1s too
-
+			if(NV_mtrs[i] == NV_sentinel) break; //Ok, we've reached the sentinel -- all the remaining empty spots are filled with sentinels too
+			writeDebugStreamLine("%d %d",NV_mtrs[i],motor[NV_mtrs[i]]);
 			if(!NV_mode_nonviolent) motor[NV_mtrs[i]] = NV_targets[i]; //If we've set nonviolent mode to off, just set the motor directly.
 
 			diff = NV_targets[i] - motor[NV_mtrs[i]];
